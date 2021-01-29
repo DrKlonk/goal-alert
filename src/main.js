300f9dd99a026514fc14763281db8cf63a928927
DrKlonk <joranminjon@gmail.com>
2021-01-29 11:59:33 +0100

    added server for heroku

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueMaterial from 'vue-material'
 import App from './App.vue'
 
 import 'vue-material/dist/vue-material.min.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
