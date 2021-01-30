0aaa4dde5d9597987849ec747903d6fced7eabab
DrKlonk <joranminjon@gmail.com>
2021-01-31 00:50:34 +0100

    add subscription to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express')
+const webPush = require('web-push')
+const bodyParser = require('body-parser')
 const serveStatic = require('serve-static')
 const path = require('path')
+const mongoose = require('mongoose')
+require('dotenv/config')
+const Subscription = require('./Subscription')
+
+const privateKey = process.env.VAPID_PRIVATE_KEY
+const publicKey = 'BNyoiU3hiaZIehXTmGaR5b-TznoGbqQZoG9uMaJ-0JNZIto-EHPeu-WN1TX2Wqj4HjuIduxm6QoxZZJimTmRjKo'
+webPush.setVapidDetails('mailto:joranminjon@gmail.com', publicKey, privateKey)
 
 const app = express()
+app.use(bodyParser.json())
 app.use(serveStatic(path.join(__dirname, 'dist')))
+
+// Connect to db
+mongoose.connect(
+  process.env.MONGOLAB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => console.log('Connected to Mongo DB!'))
+
+app.post('/subscribe', (req, res) => {
+  const subscription = new Subscription({
+    ...req.body
+  })
+  subscription.save()
+    .then(data => {
+      console.log('Subscription saved!')
+      return res.json(data)
+    })
+    .catch(err => console.error(err))
+
+  // res.status(201).json({})
+  console.log('Subscribed!')
+})
 
 const port = process.env.PORT || 5001
 app.listen(port, () => {
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,16 +2,52 @@
 
 import { register } from 'register-service-worker'
 
+const publicKey = 'BNyoiU3hiaZIehXTmGaR5b-TznoGbqQZoG9uMaJ-0JNZIto-EHPeu-WN1TX2Wqj4HjuIduxm6QoxZZJimTmRjKo'
+
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
+        'App is being served from cache by a service worker.'
       )
 
+      // Get permission for notifications on startup
       Notification.requestPermission(status => {
-        console.log(status)
+        if (status === 'granted') {
+          // Add the permission to the database
+          let reg
+          navigator.serviceWorker.ready
+            .then(swReg => {
+              reg = swReg
+              return swReg.pushManager.getSubscription()
+            })
+            .then(existingSub => {
+              if (existingSub !== null) {
+                console.log('Already subscribed')
+                return null
+              }
+              console.log('Creating a new subscription')
+              return reg.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(publicKey)
+              })
+            })
+            .then(newSub => {
+              if (newSub == null) {
+                return
+              }
+              console.log('Adding new subscription to MongoDB')
+              fetch('/subscribe', {
+                method: 'POST',
+                body: JSON.stringify(newSub),
+                headers: { 'Content-Type': 'application/json' }
+              })
+              // newSub.notify(new Notification('Bedankt voor je subscription', {
+              //   body: 'sdf'
+              // }))
+            })
+            .catch(err => console.error(err))
+        }
       })
     },
     registered () {
@@ -33,4 +69,19 @@ if (process.env.NODE_ENV === 'production') {
       console.error('Error during service worker registration:', error)
     }
   })
+}
+
+function urlBase64ToUint8Array (base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4)
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/')
+
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
 }
