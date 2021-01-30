f633dd013f860a32af4f5931d0c1b985db8fbab4
DrKlonk <joranminjon@gmail.com>
2021-01-31 00:09:56 +0100

    push notification from devtools

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,3 +5,28 @@ workbox.setConfig({
 })
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+let clickOpenUrl
+
+self.addEventListener('push', event => {
+  clickOpenUrl = 'https://goal-alert.herokuapp.com'
+  const options = {
+    body: event.data.text(),
+    icon: './img/icons/apple-touch-icon.png',
+    image: './test.jpg',
+    vibrate: [200, 100, 200, 100, 200],
+    tag: 'vibration-sample'
+  }
+  event.waitUntil(
+    self.registration.showNotification('title', options)
+  )
+})
+
+self.addEventListener('notificationclick', event => {
+  const clickedNotification = event.notification
+  clickedNotification.close()
+  if (clickOpenUrl) {
+    const promiseChain = clients.openWindow(clickOpenUrl)
+    event.waitUntil(promiseChain)
+  }
+})
